cfe2e5329a2021553cce9e57ba0ea0f0061bd663
invictusmortis <Invictusmortis@gmail.com>
2018-02-06 19:08:22 +0200

    add routing

---
+++ src/containers/Chat.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+class Chat extends Component {
+  render() {
+    return (
+      <div> Chat </div>
+    );
+  }
+}
+
+export default Chat;
---
+++ src/containers/Welcome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div> kyky </div>
+    );
+  }
+}
+
+export default Welcome;
